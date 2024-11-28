ccaba7549c0b8c13536f0fcd4ace1792fd920186
Sooraj-Rao <soorajrao630@gmail.com>
2024-11-28 20:44:44 +0530

    update2

--- src/app/api/getAll/route.ts
+++ src/app/api/getAll/route.ts
@@ -3,34 +3,41 @@ import { View } from "@/app/lib/model";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
-  const { url } = await request.json();
-  const responseHeaders = {
-    "Content-Type": "application/json",
+  const headers = new Headers({
     "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET",
+    "Access-Control-Allow-Methods": "POST, OPTIONS",
     "Access-Control-Allow-Headers": "Content-Type, Authorization",
-    "Cache-Control": "no-cache, no-store, must-revalidate",
-  };
+  });
 
   if (request.method === "OPTIONS") {
-    return new NextResponse(null, {
-      status: 200,
-      headers: responseHeaders,
-    });
+    return new NextResponse(null, { headers, status: 204 });
   }
+
+  const { url } = await request.json();
 
   await ConnectDb();
   if (url !== process.env.OWNER) {
-    return new NextResponse("Nigga", { status: 401 });
+    return new NextResponse("Unauthorized", { status: 401, headers });
   }
 
   try {
     const existingView = await View.find();
-    return NextResponse.json({ data: existingView, error: null });
+    return NextResponse.json({ data: existingView, error: null }, { headers });
   } catch (error) {
     console.error("Failed to get All:", error);
     return new NextResponse("Failed to get All", {
       status: 500,
+      headers,
     });
   }
+}
+
+export async function OPTIONS() {
+  const headers = new Headers({
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "POST, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
+  });
+
+  return new NextResponse(null, { headers, status: 204 });
 }
