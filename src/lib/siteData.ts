db222c2b40d2ac4359f2b8b9550a667b8b54e75f
Sooraj Rao <143273952+Sooraj-Rao@users.noreply.github.com>
2025-06-19 18:50:12 +0530

    Update

--- src/lib/siteData.ts
+++ src/lib/siteData.ts
@@ -2,7 +2,7 @@ export const siteData = {
   author: "Sooraj Rao",
   siteName: "ViewCounter",
   desc: "Highly customizable view counter for your profiles and pages!",
-  portfolio: "https://soorajrao.in?ref=ViewCounter_srj18",
+  portfolio: "https://soorajrao.in?ref=ViewCounter",
   email: "mailto:soorajrao630@gmail.com",
   linkedin: "https://www.linkedin.com/in/sooraj-rao",
   report: "https://contact.soorajrao.in/?querytype=report&utm_source=ViewCounter&message=I want to report an issue in your viewcounter app&utm_mdedium=",
